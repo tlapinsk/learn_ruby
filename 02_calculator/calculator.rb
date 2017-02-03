5256a2a664cc4844537e3c72913ec537a03912b9
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-02-03 12:00:42 -0800

    Pass

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
-#write your code here
+def add(a, b)
+	add = a + b
+end
+
+def subtract(a, b)
+	subtract = a - b
+end
+
+
+def sum(a)
+	a.inject(0) {|tot, x| tot += x}
+end
+
+def multiply(a)
+	a.inject(:*)
+end
+
+def power(a, b)
+	power = a ** b
+end
+
+def factorial(n)
+	(1..n).inject(:*) || 1
+end
