5310a0d6bbb0e1eed1ebba509e3155a819fc7c76
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-02-05 21:24:23 -0800

    Pass

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
-#write your code here
+def echo(n)
+	n
+end
+
+def shout(n)
+	n.upcase
+end
+
+def repeat(input, n=2)
+  ([input] * n).join ' '
+end
+
+def start_of_word(input, n)
+  input[0...n]
+end
+
+def first_word(input)
+	input.partition(" ").first
+end
+
+def titleize(input)
+  input[0] = input[0].capitalize
+  b = ["and", "the", "over"]
+  s = input.split.map{ |i| b.include?(i) ? i : i.capitalize }
+  return s.join(" ")
+end
