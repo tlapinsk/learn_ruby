3bf935a713041cdbeffdd27aafb61ed5e6b0a8c9
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-02-07 20:32:58 -0800

    Pass

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string (time=@seconds)
+  	hours = time / 3600
+    remainder = time % 3600
+  	minutes = remainder / 60
+  	seconds = remainder % 60
+
+  	final(hours)+ ":" +final(minutes) + ":" + final(seconds) 
+  end
+
+  def final num
+  	if num < 10
+	  "0" + num.to_s
+	else
+	  num.to_s
+	end
+  end
 end
