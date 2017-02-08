1c6f82fde00ddc5868c464fb3d791c361cc9eb00
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-02-07 19:58:47 -0800

    Pass

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
-# write your code here
+	
+	attr_accessor :title
+
+	def title
+	  b = ["a", "an", "and", "over", "the", "in", "of"]
+	  s = @title.split.map{ |i| b.include?(i) ? i : i.capitalize }
+	  @title = s.join(" ")
+	  @title[0] = @title[0].capitalize
+	  @title
+	end
 end
